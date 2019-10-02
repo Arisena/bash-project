19fd87800ef12e8b2b1afb6551ddc6c237e4b6ff
Arisena <bcmccourt202@gmail.com>
2019-10-02 15:53:37 -0500

    Started

--- user.sh
+++ user.sh
@@ -10,3 +10,51 @@ cyan='\e[36m'
 gray='\e[37m'
 white='\e[0m'
 
+clear
+
+while [ -z "$go" ]
+do
+
+menu=( '1. Create User' '2. Change User Group' '3. Create Group' '4. Delete User' '5. Change Password' '6. Return to Main Menu' '7. Shutdown'  )
+
+echo "Numbers Only"
+
+echo "--UserOperations--"
+for element in "${menu[@]}"
+do
+	echo ": $element"
+done
+echo "------------------"
+
+read -p "choice: " choice
+lower=${choice,,}
+
+case $lower in
+	#Create User
+	1)
+		;;
+	#Change User Group
+	2)
+		;;
+	#Create Group
+	3)
+		;;
+	#Delete User
+	4)
+		;;
+	#Change Password
+	5)
+		;;
+	6)
+		exit
+		;;
+	7)
+		shutdown
+		;;
+	*)
+		echo "Invalid Input"
+		sleep 4
+		clear
+		;;
+esac
+done
