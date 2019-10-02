0db0aa39a79d47115cfbfdc819c92368d08ea268
Arisena <bcmccourt202@gmail.com>
2019-10-02 14:18:38 -0500

    Finished Symlink

--- file.sh
+++ file.sh
@@ -59,8 +59,9 @@ case $lower in
 		echo "Please use full path"
 		read -p $'What directory would you like to create? ' dir
 		mkdir $dir
-		if [[ -f $(bash -c "echo $file") ]]; then
+		if [[ -d $(bash -c "echo $dir") ]]; then
 			echo "Directory Created"
+			pwd $dir
 			sleep 4
 			clear
 		else
@@ -71,9 +72,49 @@ case $lower in
 		;;
 	#Delete Directory
 	4)
+		echo "Directory Removal"
+		echo "Please us full path"
+		echo "Please not directory must be empty"
+		read -p $'What directory would you like to delete? ' dir
+		if [[ -d $(bash -c "echo $dir") ]]; then
+			rmdir $dir
+			echo "Directory Deleted"
+			sleep 4
+			clear
+		else
+			echo "Failed to remove directory"
+			sleep 4
+			clear
+		fi
 		;;
 	#Create Symlink
 	5)
+		exist=0
+		echo "Symlink Creation"
+		echo "Full path is needed here"
+		while [ $exist -eq 0 ]
+		do
+		read -p $'What file are we symlinking today? ' file
+		if [[ -f $(bash -c "echo $file") ]]; then
+			exist=1
+			echo "File exists"
+		elif [[ -d $(bash -c "echo $file") ]]; then
+			exist=1
+			echo "Directory exists"
+		else
+			echo "File does not exist"
+		fi
+		done
+		read -p $'Where would you like the symlink to be? ' link
+		ln -s $file $link
+		if [[ -e $(bash -c "echo $link") ]]; then
+			echo "Creation Successful"
+			ls $link
+			sleep 4
+			clear
+		else
+			echo "Creation failed"
+		fi
 		;;
 	#Change Ownership
 	6)
