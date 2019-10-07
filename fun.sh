0e15d5085bfb191933dc56aa4980eb9a6020101d
Arisena <34319484+Arisena@users.noreply.github.com>
2019-10-07 13:26:24 -0500

    added more stuff

--- fun.sh
+++ fun.sh
@@ -12,7 +12,12 @@ cyan='\e[36m'
 gray='\e[37m'
 white='\e[0m'
 
-menu=( '1. Fortune' '2. sl' '3. cmatrix' 'Return to Main Menu' 'Shutdown' )
+clear
+
+while [ -z $go ]
+do
+
+menu=( '1. Fortune' '2. sl' '3. cmatrix' '4. Ghostbusters' '5. figlet banner' 'Return to Main Menu' 'Shutdown' )
 
 echo -e $gray"Numbers Only"
 
@@ -42,10 +47,22 @@ case $lower in
 	3)
 		cmatrix
 		;;
+	#cowsay Ghostbusters
 	4)
+		cowsay -f ghostbusters Who you Gonna Call
 		;;
+	#figlet
 	5)
+		printf "What would you like the text to be? "
+		read text
+		figlet $text
+		printf "Output printed to 'figlet_output.txt'\n"
+		figlet $text >> figlet_output.txt
+		printf "Type anything to continue"
+		read
+		clear
 		;;
 	6)
 		;;
 esac
+done
