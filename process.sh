145d76ef2df5123964509941be7deb9bd133bce1
Arisena <bcmccourt202@gmail.com>
2019-10-07 20:01:35 -0500

    started

--- process.sh
+++ process.sh
@@ -12,3 +12,17 @@ cyan='\e[36m'
 gray='\e[37m'
 white='\e[0m'
 
+menu=( '1. View all Processes' '2. Kill a Process' '3. Top' '4. Change Process Priority' '5. Return to Main Menu' '6. Shutdown' )
+
+echo -e $gray"Numbers Only"
+
+echo -e "$gray-FileOperations-"
+for element in "${menu[@]}"
+do
+	echo -e "$gray:$yellow $element"
+done
+echo -e "$gray----------------$white"
+
+printf "Choice: "
+read choice
+lower=${choice,,}
