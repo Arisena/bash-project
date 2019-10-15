7fb682fc1ad18401ab9c1da57a680d421d45ea12
Arisena <34319484+Arisena@users.noreply.github.com>
2019-10-15 10:30:24 -0500

    Implemented Man Section

--- locate.sh
+++ locate.sh
@@ -45,13 +45,13 @@ case $lower in
 			printf "What file would you like to search? "
 			read file
 			if [[ -f $(bash -c "echo $file") ]]; then
-				echo -e "File Found"
+				echo -e $green"File Found"
 				exist=1
 			else
-				echo -e "Failed to find file"
+				echo -e $red"Failed to find file"
 			fi
 		done
-		echo -e "Opening search results"
+		echo -e $white"Opening search results"
 		sleep 1
 		grep --color=auto -rn $text $file | less
 		echo -e $green"Output will be into search.txt for later use$white"
@@ -107,6 +107,54 @@ case $lower in
 		;;
 	#Man Pages
 	4)
+		clear
+		exists=0
+		manpage=0
+		menu2=( '1. View Available Pages' '2. Choose Page' )
+
+		while [ $exists -eq 0 ]
+		do
+		echo -e $gray"--------ManViewer--------"
+		for element in "${menu2[@]}"
+		do
+			echo -e "$gray:$yellow $element"
+		done
+		echo -e "$gray-------------------------$white"
+		printf "Choice: "
+		read choice
+		lower=${choice,,}
+
+		case $lower in
+			1)
+				man -k . | less
+				clear
+				;;
+			2)
+				printf "Command is case sensitive\n"
+				printf "If no man page is loaded no page was found\n"
+				sleep 1
+				while [ $manpage = 0 ]
+				do
+					printf $white"What man page would you like to view? "
+					read page
+					if man $page; then
+						manpage=1
+					else
+						echo -e $red"No Page Found"
+					fi
+				done
+				printf "Press Enter to Continue"
+				read
+				exists=1
+				clear
+				;;
+			*)
+				printf $red"Invalid Option"
+				sleep 2
+				clear
+				;;
+		esac
+		done
 		;;
 	5)
 		exit
