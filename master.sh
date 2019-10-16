ef9cba1654ea3140f51db210bada3470a5976e1a
Arisena <bcmccourt202@gmail.com>
2019-10-15 19:50:59 -0500

    centered text

--- master.sh
+++ master.sh
@@ -12,16 +12,53 @@ cyan='\e[36m'
 gray='\e[37m'
 white='\e[0m'
 
-center() {
-  termwidth="$(tput cols)"
-  padding="$(printf '%0.1s' ={1..500})"
-  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+center()
+{
+    local terminal_width=$(tput cols)     # query the Terminfo database: number of columns
+    local text="${1:?}"                   # text to center
+    local glyph="${2:-=}"                 # glyph to compose the border
+    local padding="${3:-2}"               # spacing around the text
+
+    local text_width=${#text}             
+
+    local border_width=$(( (terminal_width - (padding * 2) - text_width) / 2 ))
+
+    local border=                         # shape of the border
+
+    # create the border (left side or right side)
+    for ((i=0; i<border_width; i++))
+    do
+        border+="${glyph}"
+    done
+
+    # a side of the border may be longer (e.g. the right border)
+    if (( ( terminal_width - ( padding * 2 ) - text_width ) % 2 == 0 ))
+    then
+        # the left and right borders have the same width
+        local left_border=$border
+        local right_border=$left_border
+    else
+        # the right border has one more character than the left border
+        # the text is aligned leftmost
+        local left_border=$border
+        local right_border="${border}${glyph}"
+    fi
+
+    # space between the text and borders
+    local spacing=
+
+    for ((i=0; i<$padding; i++))
+    do
+        spacing+=" "
+    done
+
+    # displays the text in the center of the screen, surrounded by borders.
+    printf "${left_border}${spacing}${text}${spacing}${right_border}\n"
 }
 
 clear
-printf $gray
-center "Warning, some options need you to be an admin"
-center "Please consult system administrator if something fails"
+center $gray"Warning, some options need you to be an admin"$white " "
+center $gray"Please consult system administrator if something fails"$white " "
 sleep 3
 
 while [ -z "$go" ]
